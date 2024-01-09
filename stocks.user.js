111eece1903d247836ccdfd7c6551480287a59d3
nazarfrenk77 <nazarfrenk77@gmail.com>
2024-01-09 13:44:46 +0200

    Create template

---
+++ stocks.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name        HWM_Stocks
+// @description For hwm stocks 
+// @author      Nazar Franko
+// @license     MIT
+// @version     1.0
+// @include     *my.lordswm.com/
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
